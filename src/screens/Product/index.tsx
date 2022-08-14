53b3737a4061edfda5a28c06830ce943a4233bbb
João Oliveira Lima Júnior <joao.jbsb@gmail.com>
2022-08-13 22:42:27 -0300

    ImagePicker

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,14 +1,28 @@
 import React, {useState} from 'react';
-import { Platform, TouchableOpacity } from 'react-native';
+import { Platform, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+
+import firestore from '@react-native-firebase/firestore'; //para armazenar informações da pizza
+import storage from '@react-native-firebase/storage'; //para fazer upload da imagem
 
 import { ButtonBack } from '../../components/ButtonBack';
 import { Photo } from '../../components/Photo';
+import { InputPrice } from '../../components/InputPrice';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
 
-import { Container, Header, Title, DeleteLabel, Upload, PickImageButton } from './styles';
+import { Container, Header, Title, DeleteLabel, Upload, PickImageButton, 
+          Form, Label, InputGroup, InputGroupHeader, MaxCharacters} from './styles';
 
 export function Product() {
     const [image,setImage] = useState('');
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [priceSizeP, setPriceSizeP] = useState('');
+    const [priceSizeM, setPriceSizeM] = useState('');
+    const [priceSizeG, setPriceSizeG] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
 
     async function handlePickerImage(){
         const { status } = await ImagePicker.requestCameraPermissionsAsync();
@@ -23,22 +37,125 @@ export function Product() {
                 setImage(result.uri);
             }
         }
-    }
+    };
+
+    async function handleAdd(){
+        //verificar se está preenhendo os campos antes de salvar, o trim faz com que não aceite
+        //espaços em branco.
+
+        if(!name.trim()){
+            return Alert.alert('Cadastro', 'Informe o nome da pizza');
+        }
+
+        if(!description.trim()){
+            return Alert.alert('Cadastro', 'Informe a descrição da pizza');
+        }
+
+        if(!image){
+            return Alert.alert('Cadastro', 'Selecione a imagem da pizza.');
+        }
+
+        if(!priceSizeP || !priceSizeM || !priceSizeG){
+            return Alert.alert('Cadastro', 'Informe o tamanho e preço da pizza.');
+        }
+
+        setIsLoading(true);
+        //fazendo upload das imagens
+
+        const fileName = new Date().getTime();
+        const reference = storage().ref(`/pizzas/${fileName}.png`);
+        
+        await reference.putFile(image);
+        const photo_url = await reference.getDownloadURL();
+
+        firestore()
+        .collection('pizzas')
+        .add({
+            name,
+            name_insensitive: name.toLowerCase().trim(),
+            description,
+            prices_size: {
+                p: priceSizeP,
+                m: priceSizeM,
+                g: priceSizeG
+            },
+            photo_url,
+            photo_path: reference.fullPath
+        })
+        .then(() => Alert.alert('Cadastro', 'Pizza cadastrada com sucesso.'))
+        .catch(()=> Alert.alert('Cadastro', 'Não foi possível cadastrar a pizza.'))
+
+        setIsLoading(false);
+    };
+
 
     return (
         <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined} >
-            <Header>
-                <ButtonBack />
-                <Title>Cadastrar</Title>
-                <TouchableOpacity>
-                    <DeleteLabel>Deletar</DeleteLabel>
-                </TouchableOpacity>
-            </Header>
+            <ScrollView>
+                <Header>
+                    <ButtonBack />
+                    <Title>Cadastrar</Title>
+                    <TouchableOpacity>
+                        <DeleteLabel>Deletar</DeleteLabel>
+                    </TouchableOpacity>
+                </Header>
 
-            <Upload>
-                <Photo uri={image} />
-                <PickImageButton title="Carregar" type="secondary" onPress={handlePickerImage} />
-            </Upload>
+                <Upload>
+                    <Photo uri={image} />
+                    <PickImageButton title="Carregar" type="secondary" onPress={handlePickerImage} />
+                </Upload>
+
+                <Form>
+                    <InputGroup>
+                        <Label>Nome</Label>
+                        <Input 
+                            onChangeText={setName}
+                            value={name}
+                        />
+                    </InputGroup>
+
+                    <InputGroup>
+                        <InputGroupHeader>
+                            <Label>Descrição</Label>
+                            <MaxCharacters> 0 de 60 caracteres </MaxCharacters>
+                        </InputGroupHeader>
+
+                        <Input 
+                            multiline
+                            maxLength={60}
+                            style={{height: 80}}
+                            onChangeText={setDescription}
+                            value={description}
+                        />
+                    </InputGroup>
+                </Form>
+
+                <InputGroup>
+                    <Label>Tamanho e Preços</Label>
+                    <InputPrice 
+                        size="P" 
+                        onChangeText={setPriceSizeP}
+                        value={priceSizeP}
+                    />
+                    <InputPrice 
+                        size="M" 
+                        onChangeText={setPriceSizeM}
+                        value={priceSizeM}
+                    />
+                    <InputPrice 
+                        size="G"
+                        onChangeText={setPriceSizeG}
+                        value={priceSizeG}
+                    />
+                </InputGroup>
+
+                <Button 
+                    title='Cadastrar Pizza'
+                    isLoading={isLoading}
+                    onPress={handleAdd}
+                />
+
+            </ScrollView>
 
         </Container>
     )
--- src/screens/Product/styles.ts
+++ src/screens/Product/styles.ts
@@ -25,8 +25,8 @@ export const Title = styled.Text`
     font-size: 24px;
     ${({ theme }) => css`
         font-family: ${theme.FONTS.TITLE};
-        color: ${theme.COLORS.TITLE}
-    `}
+        color: ${theme.COLORS.TITLE};
+    `};
 `;
 
 export const DeleteLabel = styled.Text`
@@ -48,4 +48,41 @@ export const Upload = styled.View`
 export const PickImageButton = styled(Button)`
     max-width: 90px;
     margin-left: 32px;
+`;
+
+export const Form = styled.View`
+    width: 100%;
+    padding: 24px;
+`;
+
+export const Label = styled.Text`
+    margin-bottom: 12px;
+    font-size: 14px;
+
+    ${({ theme })=> css`
+        font-family: ${theme.FONTS.TEXT};
+        color: ${theme.COLORS.SECONDARY_900};
+    `};
+`;
+
+export const InputGroup = styled.View`
+    width: 100%;
+    margin-bottom: 16px;
+`;
+
+export const InputGroupHeader = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const MaxCharacters = styled.Text`
+    font-size: 10px;
+    margin-bottom: 12px;
+
+    ${({ theme }) => css`
+        font-family: ${theme.FONTS.TEXT};
+        color: ${theme.COLORS.SECONDARY_900};
+    `};
 `;
